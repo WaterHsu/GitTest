f8d2c09b2ec3af109bfd7d364c9ebc0ef5848388
WaterHsu <WaterHsu@WaterHsu-PC>
2014-08-19 21:00:04 +0800

    Test

---
+++ GitTest/src/com/test/MainTest.java
@@ -0,0 +1,21 @@
+/**
+ * 秀吧网络科技有限公司版权所有
+ * Copyright (C) xiu8 Corporation. All Rights Reserved
+ */
+package com.test;
+
+/**
+ * @author WaterHsu@xiu8.com
+ * @version 2014年8月19日
+ */
+public class MainTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
