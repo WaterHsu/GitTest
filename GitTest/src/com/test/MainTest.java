690022e8a1598a03fabb542e9b145de412f63ec0
ChakhinHsui <hsuichakhin@gmail.com>
2014-10-14 16:38:40 +0800

    first 11

--- GitTest/src/com/test/MainTest.java
+++ GitTest/src/com/test/MainTest.java
@@ -15,7 +15,8 @@ public class MainTest {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+	//	System.out.println("Hello World!");
+		System.out.println("Hello XPF");
 	}
 
 }
